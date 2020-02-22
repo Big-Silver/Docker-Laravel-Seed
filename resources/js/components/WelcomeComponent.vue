9183b8ecd2c0daee9591eaceae35644dffb42fa0
big-silver <bigsilversstar@yandex.com>
2020-02-22 15:37:31 +0000

    Add the vue component

---
+++ resources/js/components/WelcomeComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="masthead d-flex">
+    <div class="container text-center my-auto">
+      <h1 class="mb-1">Laravel Seed</h1>
+      <h3 class="mb-5">
+        <em>A Free Bootstrap Theme by Start Bootstrap</em>
+      </h3>
+      <a
+        class="btn btn-primary btn-xl js-scroll-trigger"
+        href="https://laravel.com/docs/6.x"
+        target="_blank"
+      >View Document</a>
+    </div>
+    <div class="overlay"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+  }
+};
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="masthead d-flex">
-    <div class="container text-center my-auto">
-        <h1 class="mb-1">Laravel Seed</h1>
-        <h3 class="mb-5">
-            <em>A Free Bootstrap Theme by Start Bootstrap</em>
-        </h3>
-        <a class="btn btn-primary btn-xl js-scroll-trigger" href="https://laravel.com/docs/6.x" target="_blank">View Document</a>
-    </div>
-    <div class="overlay"></div>
-</div>
+<welcome-component></welcome-component>
 @endsection
